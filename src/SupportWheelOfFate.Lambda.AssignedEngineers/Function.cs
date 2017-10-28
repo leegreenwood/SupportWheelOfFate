afa6e377d24def041abadd6f0f846ca5eacd5f13
Lee Greenwood <lee.greenwood@gmail.com>
2017-10-28 16:12:25 +0100

    Return Engineers List via API

--- src/SupportWheelOfFate.Lambda.AssignedEngineers/Function.cs
+++ src/SupportWheelOfFate.Lambda.AssignedEngineers/Function.cs
@@ -1,7 +1,9 @@
 
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 using SupportWheelOfFate.Lambda.AssignedEngineers.Models;
 using System.Collections.Generic;
+using System.Net.Http;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -10,7 +12,7 @@ namespace SupportWheelOfFate.Lambda.AssignedEngineers
 {
     public class Function
     {
-        
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
@@ -21,14 +23,25 @@ namespace SupportWheelOfFate.Lambda.AssignedEngineers
         {
             var engineers = new List<Engineer>();
 
-            engineers.Add(new Engineer
+            var url = "https://fj7w0figk9.execute-api.us-east-1.amazonaws.com/prod/engineers/"; // Obtain list of all Engineers
+
+            using (var client = new HttpClient())
             {
-                EngineerName = "Rich Purnell",
-                EngineerHandle = "Steely Eyed Rocket Man",
-                EngineerId = "6632b5d5-e656-4b27-9ea1-1046de9eb4c8",
-                DateLastShift = System.DateTime.UtcNow.ToString("yyyy-MM-dd"),
-                TimeLastShift = "AM"
-            });
+                var apiResponse = client.GetAsync(url).Result;
+
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    // by calling .Result you are performing a synchronous call
+                    var responseContent = apiResponse.Content;
+
+                    // by calling .Result you are synchronously reading the result
+                    string json = responseContent.ReadAsStringAsync().Result;
+
+                    engineers = JsonConvert.DeserializeObject<List<Engineer>>(json);                    
+                }
+            }
+
+            // TODO: Need to correctly process the Engineers List
 
             return engineers;
         }
