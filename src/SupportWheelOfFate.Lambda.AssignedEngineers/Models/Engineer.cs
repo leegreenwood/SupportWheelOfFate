d2e7d4d7630bec7b32e8617560074324c58ec142
Lee Greenwood <lee.greenwood@gmail.com>
2017-10-28 13:51:55 +0100

    Add serialization settings

--- src/SupportWheelOfFate.Lambda.AssignedEngineers/Models/Engineer.cs
+++ src/SupportWheelOfFate.Lambda.AssignedEngineers/Models/Engineer.cs
@@ -1,15 +1,22 @@
-﻿namespace SupportWheelOfFate.Lambda.AssignedEngineers.Models
+﻿using Newtonsoft.Json;
+
+namespace SupportWheelOfFate.Lambda.AssignedEngineers.Models
 {
     public class Engineer
     {
+        [JsonProperty(PropertyName = "engineerId")]
         public string EngineerId { get; set; }
 
+        [JsonProperty(PropertyName = "engineerName")]
         public string EngineerName { get; set; }
 
-        public string EngineerHandle { get; set; }        
+        [JsonProperty(PropertyName = "engineerHandle")]
+        public string EngineerHandle { get; set; }
 
+        [JsonProperty(PropertyName = "dateLastShift")]
         public string DateLastShift { get; set; }
 
+        [JsonProperty(PropertyName = "timeLastShift")]
         public string TimeLastShift { get; set; }
     }
 }
